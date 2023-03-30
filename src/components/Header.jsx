69740877c25effef1e095446dc5cc560c6c0d975
Rayhanali-dev <rayhanalidev@gmail.com>
2023-03-31 00:37:22 +0600

    added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            <div className="logo">
-                {/* <h3>Knowledge cafe</h3> */}
-            </div>
-            <div className="header-img">
-                {/* <img src={} alt="" /> */}
+        <div className='mx-auto max-w-7xl py-6 border-b-2'>
+            <div className="flex justify-between items-center">
+                <div className="logo">
+                    <h3 className='text-3xl font-bold'>Knowledge cafe</h3>
+                </div>
+                <div className="header-img">
+                    <img className='w-16 h-16 rounded-full object-cover' src='https://images.pexels.com/photos/3772623/pexels-photo-3772623.jpeg?auto=compress&cs=tinysrgb&w=600' alt="" />
+                </div>
             </div>
         </div>
     );
