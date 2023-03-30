5e6226177191b8bb121d9fb18a8b65317cb663b5
Rayhanali-dev <rayhanalidev@gmail.com>
2023-03-31 00:17:33 +0600

    added json file and create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="logo">
+                {/* <h3>Knowledge cafe</h3> */}
+            </div>
+            <div className="header-img">
+                {/* <img src={} alt="" /> */}
+            </div>
+        </div>
+    );
+};
+
+export default Header;
